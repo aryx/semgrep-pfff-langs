aryx/semgrep-pfff-langs

commit 03636c1ffbfa9b88f181c612c3e98cc65600fc81

refactor: Drop 'trasnfo' from Tok.t (Tok.kind is the new Tok.t) (#7655)
The tranformation associated with each token is now used by Semgrep and
removing it seems to help fixing a perf regression detected in 1.19.0
(after a number of refactorings that added extra levels of indirection
in Tok-related types).

test plan:
Run Argo workflow semgrep-compare-beta.

PR checklist:

- [x] Purpose of the code is [evident to future
readers](https://semgrep.dev/docs/contributing/contributing-code/#explaining-code)
- [x] Tests included or PR comment includes a reproducible test plan
- [x] Documentation is up-to-date
- [ ] A changelog entry was [added to
changelog.d](https://semgrep.dev/docs/contributing/contributing-code/#adding-a-changelog-entry)
for any user-facing change
- [x] Change has no security implications (otherwise, ping security
team)

If you're unsure about any of this, please see:

- [Contribution
guidelines](https://semgrep.dev/docs/contributing/contributing-code)!
- [One of the more specific guides located
here](https://semgrep.dev/docs/contributing/contributing/)

libs/ast_generic/AST_generic.ml
@@ -2418,11 +2418,7 @@ class virtual ['self] iter_no_id_info =
 class virtual ['self] map_legacy =
   object (self : 'self)
     inherit [_] map
-
-    method! visit_tok _env v =
-      (* Make a copy, since Tok.t is mutable *)
-      let v = { v with Tok.token = v.token } in
-      v
+    method! visit_tok _env v = v
 
     method! visit_expr env x =
       let ekind = self#visit_expr_kind env x.e in

languages/cpp/ast/ast_cpp.ml
@@ -1029,17 +1029,13 @@ let make_expanded ii =
     ( { Tok.str = ""; pos = { charpos = 0; line = 0; column = 0; file = "" } },
       -1 )
   in
-  {
-    ii with
-    Tok.token =
-      Tok.ExpandedTok (get_original_token_location ii.Tok.token, no_virt_loc);
-  }
+  Tok.ExpandedTok (get_original_token_location ii, no_virt_loc)
 
 let make_param ?(p_name = None) ?(p_specs = []) ?(p_val = None) t =
   { p_name; p_type = t; p_specs; p_val }
 
 (* used by parsing hacks *)
-let rewrap_pinfo pi ii = { ii with Tok.token = pi }
+let rewrap_pinfo pi _ii = pi
 
 (* used while migrating the use of 'string' to 'name' in check_variables *)
 let (string_of_name_tmp : name -> string) =

languages/cpp/menhir/parse_cpp.ml
@@ -59,7 +59,7 @@ let commentized xs =
                  match s with
                  | s when s =~ "KERN_.*" -> None
                  | s when s =~ "__.*" -> None
-                 | _ -> Some ii.Tok.token)
+                 | _ -> Some ii)
              | Token_cpp.CppDirective
              | Token_cpp.CppAttr
              | Token_cpp.CppMacro ->
@@ -68,8 +68,8 @@ let commentized xs =
              | Token_cpp.CppPassingNormal
              | Token_cpp.CppPassingCosWouldGetError ->
                  raise Todo
-           else Some ii.Tok.token
-       | T.TAny_Action ii -> Some ii.Tok.token
+           else Some ii
+       | T.TAny_Action ii -> Some ii
        | _ -> None)
 
 let count_lines_commentized xs =

languages/cpp/menhir/parsing_hacks.ml
@@ -92,7 +92,7 @@ let insert_virtual_positions l =
               | ii -> Ast_cpp.rewrap_pinfo pi ii)
             x
         in
-        match ii.Tok.token with
+        match ii with
         | Tok.OriginTok _pi ->
             let prev = Tok.unsafe_loc_of_tok ii in
             loop (x :: acc) prev (strlen ii) xs
@@ -111,7 +111,7 @@ let insert_virtual_positions l =
     | [] -> List.rev acc
     | x :: xs -> (
         let ii = TH.info_of_tok x in
-        match ii.Tok.token with
+        match ii with
         | Tok.OriginTok _pi ->
             let prev = Tok.unsafe_loc_of_tok ii in
             loop (x :: acc) prev (strlen ii) xs

languages/cpp/menhir/parsing_hacks_define.ml
@@ -71,15 +71,11 @@ let pr2, _pr2_once = Common2.mk_pr2_wrappers Flag.verbose_lexing
 let mark_end_define ii =
   let tok_loc = Tok.unsafe_loc_of_tok ii in
   let ii' =
-    {
-      Tok.token =
-        Tok.OriginTok
-          {
-            str = "";
-            pos = { tok_loc.pos with charpos = Tok.bytepos_of_tok ii + 1 };
-          };
-      transfo = Tok.NoTransfo;
-    }
+    Tok.OriginTok
+      {
+        str = "";
+        pos = { tok_loc.pos with charpos = Tok.bytepos_of_tok ii + 1 };
+      }
   in
   (* fresh_tok *) TCommentNewline_DefineEndOfMacro ii'
 

languages/java/ast/ast_java.ml
@@ -500,10 +500,7 @@ type any =
 (* Helpers *)
 (*****************************************************************************)
 let unwrap = fst
-
-let fakeInfo ?(next_to = None) str =
-  { Tok.token = Tok.FakeTokStr (str, next_to); transfo = Tok.NoTransfo }
-
+let fakeInfo ?(next_to = None) str = Tok.FakeTokStr (str, next_to)
 let info_of_ident ident = snd ident
 
 let is_final xs =

languages/javascript/ast/ast_js.ml
@@ -715,4 +715,4 @@ let add_decorators_to_declarations decorators declarations =
 let fakeInfoAttach info =
   let info = Tok.rewrap_str "';' (from ASI)" info in
   let loc = Tok.unsafe_loc_of_tok info in
-  { Tok.token = Tok.FakeTokStr (";", Some (loc, -1)); transfo = Tok.NoTransfo }
+  Tok.FakeTokStr (";", Some (loc, -1))

languages/javascript/ast/visitor_js.ml
@@ -54,7 +54,7 @@ let (mk_visitor : visitor_in -> visitor_out) =
   let rec v_info x =
     let k x =
       match x with
-      | { Tok.token = _v_pinfox; transfo = _v_transfo } ->
+      | _ ->
           (*
     let arg = Parse_info.v_pinfo v_pinfox in
     let arg = v_unit v_comments in

commit 008117723be2b7e366a42e52a97344d179c9b0f7

Bump version number for Generic AST (#7674)
Fixes: 03636c1ffbf ("refactor: Drop 'trasnfo' from Tok.t (Tok.kind is
the new Tok.t) (#7655)")

test plan:
make test

PR checklist:

- [x] Purpose of the code is [evident to future
readers](https://semgrep.dev/docs/contributing/contributing-code/#explaining-code)
- [x] Tests included or PR comment includes a reproducible test plan
- [x] Documentation is up-to-date
- [x] A changelog entry was [added to
changelog.d](https://semgrep.dev/docs/contributing/contributing-code/#adding-a-changelog-entry)
for any user-facing change
- [x] Change has no security implications (otherwise, ping security
team)

If you're unsure about any of this, please see:

- [Contribution
guidelines](https://semgrep.dev/docs/contributing/contributing-code)!
- [One of the more specific guides located
here](https://semgrep.dev/docs/contributing/contributing/)

libs/ast_generic/AST_generic.ml
@@ -173,7 +173,7 @@
  * to correspond mostly to Semgrep versions. So version below can jump from
  * "1.12.1" to "1.20.0" and that's fine.
  *)
-let version = "1.19.0-2"
+let version = "1.20.0"
 
 (* Provide hash_* and hash_fold_* for the core ocaml types *)
 open Ppx_hash_lib.Std.Hash.Builtin